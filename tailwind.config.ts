7a94f6a3b45320e74edde2e87580919a5c9bf175
dharmendramicro1 <dharmendra@micro1.ai>
2024-03-11 12:36:33 +0530

    added the cloudbuild and docker file and remove the server.ts and expres js

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -85,7 +85,7 @@ module.exports = {
         "3xl":
           "0 2px 2px rgb(224 230 237 / 46%), 1px 6px 7px rgb(224 230 237 / 46%)",
       },
-      typography: ({ theme }) => ({
+      typography: ({ theme }:any) => ({
         DEFAULT: {
           css: {
             "--tw-prose-invert-headings": theme("colors.white.dark"),
